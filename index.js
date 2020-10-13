873e8d25838e1c27c554f42275a30cb20c9c3696
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2020-10-12 23:11:16 -0400

    Implement method deleting all records

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var SERVER_NAME = 'product-api';
 var PORT = 3009;
-var HOST = 'localhost';
+var HOST = '127.0.0.1';
 var getCount = 0;
 var postCount = 0;
 
@@ -9,7 +9,6 @@ var colors = require('colors');
 
 // Get a persistence engine for the products
 productsSave = require('save')('products');
-var products = [];
 
 // Create the restify server
 server = restify.createServer({ name: SERVER_NAME });
@@ -17,8 +16,8 @@ server = restify.createServer({ name: SERVER_NAME });
 server.listen(PORT, HOST, function () {
 	console.log(`Server ${server.name} listening at ${server.url}`.bold.yellow);
 	console.log('Endpoints:'.bold.brightMagenta);
-    console.log(`           ${server.url}/products`.brightMagenta);
-    console.log(`           Method: GET, POST, DELETE`.brightBlue)
+	console.log(`           ${server.url}/products`.brightMagenta);
+	console.log(`           Method: GET, POST, DELETE`.brightBlue);
 });
 
 server
@@ -26,8 +25,7 @@ server
 	.use(restify.fullResponse())
 
 	// Maps req.body to req.params so there is no switching between them
-    .use(restify.bodyParser());
-
+	.use(restify.bodyParser());
 
 // @desc       Get all products in the system
 // @endpoint   http://127.0.0.1:3009/products
@@ -42,7 +40,6 @@ server.get('/products', function (req, res, next) {
 	});
 });
 
-
 // @desc       Create a new product
 // @endpoint   http://127.0.0.1:3009/products
 // @method     POST
@@ -55,8 +52,8 @@ server.post('/products', function (req, res, next) {
 	if (req.params.price === undefined) {
 		// If there are any errors, pass them to next in the correct format
 		return next(new restify.InvalidArgumentError('price must be supplied'));
-    }
-    if (req.params.category === undefined) {
+	}
+	if (req.params.category === undefined) {
 		// If there are any errors, pass them to next in the correct format
 		return next(new restify.InvalidArgumentError('category must be supplied'));
 	}
@@ -64,7 +61,7 @@ server.post('/products', function (req, res, next) {
 		product: req.params.product,
 		price: req.params.price,
 		category: req.params.category,
-    };
+	};
 	// Create the product using the persistence engine
 	productsSave.create(newProduct, function (error, product) {
 		// If there are any errors, pass them to next in the correct format
@@ -75,6 +72,28 @@ server.post('/products', function (req, res, next) {
 
 		// Send the user if no issues
 		res.send(201, product);
-        console.log(`Processed Request Count >>>>> Post: ${++postCount}`.bold.green);
+		console.log(
+			`Processed Request Count >>>>> Post: ${++postCount}`.bold.green
+		);
+	});
+});
+
+// @desc       Delete all products in the system
+// @endpoint   http://127.0.0.1:3009/products
+// @method     DELETE
+server.del('/products', function (req, res, next) {
+	var query = productsSave.find({}, function (error, products) {});
+	// Find every entity within the given collection
+	console.log(`products DELETE: Deleting all products....`.green);
+	productsSave.deleteMany(query, function (error) {
+		// If there are any errors, pass them to next in the correct format
+		if (error)
+			return next(
+				new restify.InvalidArgumentError(JSON.stringify(error.errors))
+			);
+
+		// Send a 200 OK response
+		res.send(200, 'Deleted');
+		console.log(`products DELETE: Deleted all products....`.bold.red);
 	});
 });
